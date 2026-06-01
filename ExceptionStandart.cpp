3b64ec1bc825f8c2710a3b1504bf419962ce53fd
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-06-01 15:00:08 +0700

    deklarasi array data dengan panjang index 3

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -6,4 +6,9 @@ using namespace std;
 int main()
 {
     cout << "Awal Program" << endl; // Penanda 1
+    try
+    {
+        // deklarasi array data dengan panjang index 3
+        array <int, 3> data = {1, 2, 3 };
+    }
 }
