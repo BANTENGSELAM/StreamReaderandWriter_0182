403ca75525c1e1ec740029e1525855bac7f10c21
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-06-01 15:23:51 +0700

    unlimited loop untuk menulis file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -13,4 +13,10 @@ int main()
 
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, '\q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis file
+    while (true)
+
 }
